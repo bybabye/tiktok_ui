2c72f63b1ecdf70fba87846508e0a8aa33d084f4
bybabye <89548252+bybabye@users.noreply.github.com>
2022-08-17 11:54:32 +0700

    day3

---
+++ src/components/Layout/components/Header/index.js
@@ -0,0 +1,18 @@
+import classNames from 'classnames/bind';
+import style from './Header.module.scss';
+
+const cx = classNames.bind(style);
+
+function Header() {
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        {/** logo */}
+        {/** Search */}
+        {/** */}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
